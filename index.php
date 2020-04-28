564547f571735f4658b6091af5c4b7adb2623966
unknown <you@example.com>
2020-04-28 16:39:44 -0300

    tela de login funcional

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+	session_start();
 	require_once("vendor/autoload.php");
 
 	use \Slim\Slim;
 	use \Hcode\Page;
 	use \Hcode\PageAdmin;
+	use \Hcode\Model\User;
 
 	$app = new Slim();
 
@@ -19,9 +20,36 @@
 
 	$app->get('/administrador', function() {
 	    
+		User::verifyLogin();
+
 		$page = new PageAdmin();
 
 		$page->setTpl("index");
+	});
+
+	$app->get('/administrador/login', function(){
+		$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("login");
+	});
+
+	$app->post('/administrador/login', function() {
+	    
+		User::login($_POST["login"], $_POST["password"]);
+
+		header("Location: /administrador");
+		exit;
+	});
+
+	$app->get('/administrador/logout', function() {
+	    
+		User::logout();
+
+		header("Location: /administrador/login");
+		exit;
 	});
 
 	$app->run();
