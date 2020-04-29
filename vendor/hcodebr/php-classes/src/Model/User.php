55fb503dbdaccfcb5ba3aa57e92e0d5f229f8145
unknown <you@example.com>
2020-04-29 13:30:55 -0300

    criado o CRUD administrativo de usuarios

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -15,8 +15,7 @@
 			));
 			if (count($results) ===0){
 
-				throw new \Exception("Usuário inexistente ou senha invalida.");
-				
+				throw new \Exception("Usuário inexistente ou senha invalida.");			
 			}
 			$data = $results[0];
 
@@ -29,7 +28,6 @@
 				$_SESSION[USER::SESSION] = $user->getValues();
 
 				return $user;
-
 			}else{
 
 				throw new \Exception("Usuário inexistente ou senha invalida.");
@@ -56,6 +54,65 @@
 
 			$_SESSION[User::SESSION] = NULL;
 		}
-	}
 
+		public static function listAll(){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+		}
+
+		public function save(){
+
+			$sql = new Sql();
+
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
+				"desperson"=>$this->getdesperson(),
+				"deslogin"=>$this->getdeslogin(),
+				"despassword"=>$this->getdespassword(),
+				"desemail"=>$this->getdesemail(),
+				"nrphone"=>$this->getnrphone(),
+				"inadmin"=>$this->getinadmin()
+			));
+
+			$this->setData($results[0]);
+		}
+
+		public function get($iduser){
+
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+					":iduser"=>$iduser
+			));
+
+			$this->setData($results[0]);
+		}
+
+		public function update(){
+
+			$sql = new Sql();
+
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser,:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
+				"iduser"=>$this->getiduser(),
+				"desperson"=>$this->getdesperson(),
+				"deslogin"=>$this->getdeslogin(),
+				"despassword"=>$this->getdespassword(),
+				"desemail"=>$this->getdesemail(),
+				"nrphone"=>$this->getnrphone(),
+				"inadmin"=>$this->getinadmin()
+			));
+
+			$this->setData($results[0]);
+		}
+
+		public function delete(){
+
+			$sql = new Sql();
+
+			$sql->query("CALL sp_users_delete(:iduser)",array(
+				":iduser"=>$this->getiduser()
+			));
+		}
+	}
 ?>
