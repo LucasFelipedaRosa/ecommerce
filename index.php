4ff75dc06e162ede78859384343eb2284f2bf03a
unknown <you@example.com>
2020-05-11 16:12:51 -0300

    cricao do crud admin de categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	use \Hcode\Page;
 	use \Hcode\PageAdmin;
 	use \Hcode\Model\User;
+	use \Hcode\Model\Category;
 
 	$app = new Slim();
 
@@ -220,6 +221,91 @@
 		]);
 
 		$page->setTpl("forgot-reset-success");
+
+	});
+
+	$app->get("/administrador/categories", function(){
+
+		$categories = Category::listAll();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("categories",[ 
+			'categories'=>$categories
+		]);
+	});
+
+	$app->get("/administrador/categories/create", function(){
+
+		$categories = Category::listAll();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("categories-create");
+	});
+
+	$app->post("/administrador/categories/create", function(){
+
+		$categories = Category::listAll();
+
+		$category = new Category();
+
+		$category->setData($_POST);
+
+		$category->save();
+
+		header("Location: /administrador/categories");
+
+		exit;
+
+	});
+
+	$app->get("/administrador/categories/:idcategory/delete", function($idcategory){
+
+		$categories = Category::listAll();
+
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$category->delete();
+
+		header("Location: /administrador/categories");
+
+		exit;
+
+	});
+
+	$app-> get("/administrador/categories/:idcategory", function($idcategory){
+
+		$categories = Category::listAll();
+
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$page = new PageAdmin();
+
+		$page->setTpl("categories-update", [
+			'category'=>$category->getValues()
+		]);
+	});
+
+	$app->post("/administrador/categories/:idcategory", function($idcategory){
+
+		$categories = Category::listAll();
+		
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$category->setData($_POST);
+
+		$category->save();
+
+		header("Location: /administrador/categories");
+
+		exit;
 
 	});
 
