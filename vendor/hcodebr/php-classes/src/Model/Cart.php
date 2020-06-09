cbf13bf745949abc215594538531e5c507a08d0b
unknown <you@example.com>
2020-06-09 15:34:00 -0300

    carrinho Produtos

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -1,15 +1,16 @@
 <?php 
 
 	namespace Hcode\Model;
-	use Hcode\DB\Sql;
-	use Hcode\Model;
-	use Hcode\Model\User;
 
+	use \Hcode\DB\Sql;
+	use \Hcode\Model;
+	use \Hcode\Mailer;
+	use \Hcode\Model\User;
 
-	class Cart extends Model{
-
+	class Cart extends Model {
 
 		const SESSION = "Cart";
+		const SESSION_ERROR = "CartError";
 
 		public static function getFromSession()
 		{
@@ -50,6 +51,7 @@
 			}
 
 			return $cart;
+
 		}
 
 		public function setToSession()
@@ -73,46 +75,248 @@
 				$this->setData($results[0]);
 
 			}
+
 		}	
 
-		public function get(int $idcart){
+		public function get(int $idcart)
+		{
 
 			$sql = new Sql();
 
-			$results =$sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart",
-				[
-					":idcart"=>$idcart
-				]
-			);
+			$results = $sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart", [
+				':idcart'=>$idcart
+			]);
 
-			if(count($results)>0){
+			if (count($results) > 0) {
 
 				$this->setData($results[0]);
 
 			}
+
 		}
 
-		public static function listAll(){
+		public function save()
+		{
 
 			$sql = new Sql();
 
-			return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
-		}
-
-		public function save(){
-
-			$sql = new Sql();
-
-			$results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :pndays)",array(
-				":idcart"=>$this->getidcart(),
-				":dessessionid"=>$this->getdessessionid(),
-				":iduser"=>$this->getiduser(),
-				":deszipcode"=>$this->getdeszipcode(),
-				":vlfreight"=>$this->getvlfreight(),
-				":pndays"=>$this->getpndays()
-			));
+			$results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)", [
+				':idcart'=>$this->getidcart(),
+				':dessessionid'=>$this->getdessessionid(),
+				':iduser'=>$this->getiduser(),
+				':deszipcode'=>$this->getdeszipcode(),
+				':vlfreight'=>$this->getvlfreight(),
+				':nrdays'=>$this->getnrdays()
+			]);
 
 			$this->setData($results[0]);
-		}	
+
+		}
+
+		public function addProduct(Product $product)
+		{
+
+			$sql = new Sql();
+
+			$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct)", [
+				':idcart'=>$this->getidcart(),
+				':idproduct'=>$product->getidproduct()
+			]);
+
+			$this->getCalculateTotal();
+
+		}
+
+		public function removeProduct(Product $product, $all = false)
+		{
+
+			$sql = new Sql();
+
+			if ($all) {
+
+				$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+					':idcart'=>$this->getidcart(),
+					':idproduct'=>$product->getidproduct()
+				]);
+
+			} else {
+
+				$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+					':idcart'=>$this->getidcart(),
+					':idproduct'=>$product->getidproduct()
+				]);
+
+			}
+
+			$this->getCalculateTotal();
+
+		}
+
+		public function getProducts()
+		{
+
+			$sql = new Sql();
+
+			$rows = $sql->select("
+				SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+				FROM tb_cartsproducts a 
+				INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+				WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
+				GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
+				ORDER BY b.desproduct
+			", [
+				':idcart'=>$this->getidcart()
+			]);
+
+			return Product::checkList($rows);
+
+		}
+
+		public function getProductsTotals()
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd
+				FROM tb_products a
+				INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
+				WHERE b.idcart = :idcart AND dtremoved IS NULL;
+			", [
+				':idcart'=>$this->getidcart()
+			]);
+
+			if (count($results) > 0) {
+				return $results[0];
+			} else {
+				return [];
+			}
+
+		}
+
+		public function setFreight($nrzipcode)
+		{
+
+			$nrzipcode = str_replace('-', '', $nrzipcode);
+
+			$totals = $this->getProductsTotals();
+
+			if ($totals['nrqtd'] > 0) {
+
+				if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
+				if ($totals['vllength'] < 16) $totals['vllength'] = 16;
+
+				$qs = http_build_query([
+					'nCdEmpresa'=>'',
+					'sDsSenha'=>'',
+					'nCdServico'=>'40010',
+					'sCepOrigem'=>'09853120',
+					'sCepDestino'=>$nrzipcode,
+					'nVlPeso'=>$totals['vlweight'],
+					'nCdFormato'=>'1',
+					'nVlComprimento'=>$totals['vllength'],
+					'nVlAltura'=>$totals['vlheight'],
+					'nVlLargura'=>$totals['vlwidth'],
+					'nVlDiametro'=>'0',
+					'sCdMaoPropria'=>'S',
+					'nVlValorDeclarado'=>$totals['vlprice'],
+					'sCdAvisoRecebimento'=>'S'
+				]);
+
+				$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+
+				$result = $xml->Servicos->cServico;
+
+				if ($result->MsgErro != '') {
+
+					Cart::setMsgError($result->MsgErro);
+
+				} else {
+
+					Cart::clearMsgError();
+
+				}
+
+				$this->setnrdays($result->PrazoEntrega);
+				$this->setvlfreight(Cart::formatValueToDecimal($result->Valor));
+				$this->setdeszipcode($nrzipcode);
+
+				$this->save();
+
+				return $result;
+
+			} else {
+
+
+
+			}
+
+		}
+
+		public static function formatValueToDecimal($value):float
+		{
+
+			$value = str_replace('.', '', $value);
+			return str_replace(',', '.', $value);
+
+		}
+
+		public static function setMsgError($msg)
+		{
+
+			$_SESSION[Cart::SESSION_ERROR] = $msg;
+
+		}
+
+		public static function getMsgError()
+		{
+
+			$msg = (isset($_SESSION[Cart::SESSION_ERROR])) ? $_SESSION[Cart::SESSION_ERROR] : "";
+
+			Cart::clearMsgError();
+
+			return $msg;
+
+		}
+
+		public static function clearMsgError()
+		{
+
+			$_SESSION[Cart::SESSION_ERROR] = NULL;
+
+		}
+
+		public function updateFreight()
+		{
+
+			if ($this->getdeszipcode() != '') {
+
+				$this->setFreight($this->getdeszipcode());
+
+			}
+
+		}
+
+		public function getValues()
+		{
+
+			$this->getCalculateTotal();
+
+			return parent::getValues();
+
+		}
+
+		public function getCalculateTotal()
+		{
+
+			$this->updateFreight();
+
+			$totals = $this->getProductsTotals();
+
+			$this->setvlsubtotal($totals['vlprice']);
+			$this->setvltotal($totals['vlprice'] + (float)$this->getvlfreight());
+
+		}
 	}
-?>
+
+ ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -1,11 +1,12 @@
 <?php 
 
 	namespace Hcode\Model;
-	use Hcode\DB\Sql;
-	use Hcode\Model;
+
+	use \Hcode\DB\Sql;
+	use \Hcode\Model;
 	use \Hcode\Mailer;
 
-	class User extends Model{
+	class User extends Model {
 
 		const SESSION = "User";
 		const SECRET = "HcodePhp7_Secret";
@@ -62,107 +63,141 @@
 
 		}
 
-		public static function login($login, $password){
+		public static function login($login, $password)
+		{
 
 			$sql = new Sql();
-			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
-				":LOGIN"=>$login
-			));
-			if (count($results) ===0){
 
-				throw new \Exception("Usuário inexistente ou senha invalida.");			
+			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
+				":LOGIN"=>$login
+			)); 
+
+			if (count($results) === 0)
+			{
+				throw new \Exception("Usuário inexistente ou senha inválida.");
 			}
+
 			$data = $results[0];
 
-			if(password_verify($password, $data["despassword"]) === true){
+			if (password_verify($password, $data["despassword"]) === true)
+			{
 
 				$user = new User();
 
+				$data['desperson'] = utf8_encode($data['desperson']);
+
 				$user->setData($data);
 
-				$_SESSION[USER::SESSION] = $user->getValues();
+				$_SESSION[User::SESSION] = $user->getValues();
 
 				return $user;
-			}else{
 
-				throw new \Exception("Usuário inexistente ou senha invalida.");
+			} else {
+				throw new \Exception("Usuário inexistente ou senha inválida.");
 			}
+
 		}
 
-		public static function verifyLogin($inadmin = true){
+		public static function verifyLogin($inadmin = true)
+		{
 
-			if(User::CheckLogin($inadmin)){
-				header("Location: /administrador/login");
+			if (!User::checkLogin($inadmin)) {
+
+				if ($inadmin) {
+					header("Location: /admin/login");
+				} else {
+					header("Location: /login");
+				}
 				exit;
+
 			}
+
 		}
 
-		public static function logout(){
+		public static function logout()
+		{
 
 			$_SESSION[User::SESSION] = NULL;
+
 		}
 
-		public static function listAll(){
+		public static function listAll()
+		{
 
 			$sql = new Sql();
 
 			return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
 		}
 
-		public function save(){
+		public function save()
+		{
 
 			$sql = new Sql();
 
-			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
-				"desperson"=>$this->getdesperson(),
-				"deslogin"=>$this->getdeslogin(),
-				"despassword"=>$this->getdespassword(),
-				"desemail"=>$this->getdesemail(),
-				"nrphone"=>$this->getnrphone(),
-				"inadmin"=>$this->getinadmin()
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+				":desperson"=>utf8_decode($this->getdesperson()),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>User::getPasswordHash($this->getdespassword()),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
 			));
 
 			$this->setData($results[0]);
+
 		}
 
-		public function get($iduser){
+		public function get($iduser)
+		{
 
 			$sql = new Sql();
 
 			$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
-					":iduser"=>$iduser
+				":iduser"=>$iduser
 			));
 
-			$this->setData($results[0]);
+			$data = $results[0];
+
+			$data['desperson'] = utf8_encode($data['desperson']);
+
+
+			$this->setData($data);
+
 		}
 
-		public function update(){
+		public function update()
+		{
 
 			$sql = new Sql();
 
-			$results = $sql->select("CALL sp_usersupdate_save(:iduser,:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",array(
-				"iduser"=>$this->getiduser(),
-				"desperson"=>$this->getdesperson(),
-				"deslogin"=>$this->getdeslogin(),
-				"despassword"=>$this->getdespassword(),
-				"desemail"=>$this->getdesemail(),
-				"nrphone"=>$this->getnrphone(),
-				"inadmin"=>$this->getinadmin()
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+				":iduser"=>$this->getiduser(),
+				":desperson"=>utf8_decode($this->getdesperson()),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>User::getPasswordHash($this->getdespassword()),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
 			));
 
-			$this->setData($results[0]);
+			$this->setData($results[0]);		
+
 		}
 
-		public function delete(){
+		public function delete()
+		{
 
 			$sql = new Sql();
 
-			$sql->query("CALL sp_users_delete(:iduser)",array(
+			$sql->query("CALL sp_users_delete(:iduser)", array(
 				":iduser"=>$this->getiduser()
 			));
+
 		}
-	
-		public static function getForgot($email, $inadmin = true){
+
+		public static function getForgot($email, $inadmin = true)
+		{
 
 			$sql = new Sql();
 
@@ -206,8 +241,15 @@
 
 					$code = base64_encode($code);
 
+					if ($inadmin === true) {
 
-					$link = "http://www.hcodecommerce.com.br/administrador/forgot/reset?code=$code";		
+						$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+					} else {
+
+						$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+						
+					}				
 
 					$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
 						"name"=>$data['desperson'],
@@ -216,9 +258,12 @@
 
 					$mailer->send();
 
-					return $data;
+					return $link;
+
 				}
+
 			}
+
 		}
 
 		public static function validForgotDecrypt($code)
@@ -253,9 +298,11 @@
 			{
 
 				return $results[0];
-			}
-		}
 
+			}
+
+		}
+		
 		public static function setFogotUsed($idrecovery)
 		{
 
@@ -276,6 +323,95 @@
 				":password"=>$password,
 				":iduser"=>$this->getiduser()
 			));
+
+		}
+
+		public static function setError($msg)
+		{
+
+			$_SESSION[User::ERROR] = $msg;
+
+		}
+
+		public static function getError()
+		{
+
+			$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+
+			User::clearError();
+
+			return $msg;
+
+		}
+
+		public static function clearError()
+		{
+
+			$_SESSION[User::ERROR] = NULL;
+
+		}
+
+		public static function setSuccess($msg)
+		{
+
+			$_SESSION[User::SUCCESS] = $msg;
+
+		}
+
+		public static function getSuccess()
+		{
+
+			$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+			User::clearSuccess();
+
+			return $msg;
+
+		}
+
+		public static function clearSuccess()
+		{
+
+			$_SESSION[User::SUCCESS] = NULL;
+
+		}
+
+		public static function setErrorRegister($msg)
+		{
+
+			$_SESSION[User::ERROR_REGISTER] = $msg;
+
+		}
+
+		public static function getErrorRegister()
+		{
+
+			$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+			User::clearErrorRegister();
+
+			return $msg;
+
+		}
+
+		public static function clearErrorRegister()
+		{
+
+			$_SESSION[User::ERROR_REGISTER] = NULL;
+
+		}
+
+		public static function checkLoginExist($login)
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+				':deslogin'=>$login
+			]);
+
+			return (count($results) > 0);
+
 		}
 
 		public static function getPasswordHash($password)
@@ -286,5 +422,79 @@
 			]);
 
 		}
+
+		public function getOrders()
+		{
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT * 
+				FROM tb_orders a 
+				INNER JOIN tb_ordersstatus b USING(idstatus) 
+				INNER JOIN tb_carts c USING(idcart)
+				INNER JOIN tb_users d ON d.iduser = a.iduser
+				INNER JOIN tb_addresses e USING(idaddress)
+				INNER JOIN tb_persons f ON f.idperson = d.idperson
+				WHERE a.iduser = :iduser
+			", [
+				':iduser'=>$this->getiduser()
+			]);
+
+			return $results;
+
+		}
+
+		public static function getPage($page = 1, $itemsPerPage = 10)
+		{
+
+			$start = ($page - 1) * $itemsPerPage;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS *
+				FROM tb_users a 
+				INNER JOIN tb_persons b USING(idperson) 
+				ORDER BY b.desperson
+				LIMIT $start, $itemsPerPage;
+			");
+
+			$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+			return [
+				'data'=>$results,
+				'total'=>(int)$resultTotal[0]["nrtotal"],
+				'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+			];
+
+		}
+
+		public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+		{
+
+			$start = ($page - 1) * $itemsPerPage;
+
+			$sql = new Sql();
+
+			$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS *
+				FROM tb_users a 
+				INNER JOIN tb_persons b USING(idperson)
+				WHERE b.desperson LIKE :search OR b.desemail = :search OR a.deslogin LIKE :search
+				ORDER BY b.desperson
+				LIMIT $start, $itemsPerPage;
+			", [
+				':search'=>'%'.$search.'%'
+			]);
+
+			$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+			return [
+				'data'=>$results,
+				'total'=>(int)$resultTotal[0]["nrtotal"],
+				'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+			];
+		} 
 	}
-?>
+ ?>
